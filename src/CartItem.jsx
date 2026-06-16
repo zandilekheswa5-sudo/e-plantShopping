2634d766590176de30c8fa7945c6f29e0377dd19
Zandile Kheswa <zandilekheswa5@gmail.com>
2026-06-16 22:08:07 +0200

    Complete Redux shopping cart project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,48 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => {
+      return total + parseInt(item.cost.replace('$', '')) * item.quantity;
+    }, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        quantity: item.quantity + 1,
+      })
+    )
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1,
+      })
+    );
+   } else {
+     dispatch(removeItem(item.name));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    console.log(item);
+
+    const price = Number(item.cost.replace('$', ''));
+    return price * item.quantity;
   };
 
   return (
